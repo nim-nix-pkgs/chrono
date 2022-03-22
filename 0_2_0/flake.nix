7ab2bc39e09f76c5ac20334bc364845bbf67f6b5
hugosenari <hugosenari@gmail.com>
2022-03-22 00:25:56 -0300

    chore: re-index ../nimpkgs/c/chrono/0_2_0

--- 0_2_0/flake.nix
+++ 0_2_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-chrono-0_2_0.flake = false;
   inputs.src-chrono-0_2_0.owner = "treeform";
-  inputs.src-chrono-0_2_0.ref   = "refs/tags/0.2.0";
+  inputs.src-chrono-0_2_0.ref   = "0_2_0";
   inputs.src-chrono-0_2_0.repo  = "chrono";
   inputs.src-chrono-0_2_0.type  = "github";
   
